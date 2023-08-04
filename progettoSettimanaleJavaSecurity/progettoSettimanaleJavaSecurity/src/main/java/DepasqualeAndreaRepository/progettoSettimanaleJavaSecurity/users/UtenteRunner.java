959041dd941b9e78b1add706d1116ac8aa861759
Andrea De Pasquale <128024931+DepasqualeAndrea@users.noreply.github.com>
2023-08-04 17:21:04 +0200

    interamente funzionale

--- progettoSettimanaleJavaSecurity/progettoSettimanaleJavaSecurity/src/main/java/DepasqualeAndreaRepository/progettoSettimanaleJavaSecurity/users/UtenteRunner.java
+++ progettoSettimanaleJavaSecurity/progettoSettimanaleJavaSecurity/src/main/java/DepasqualeAndreaRepository/progettoSettimanaleJavaSecurity/users/UtenteRunner.java
@@ -1,10 +1,15 @@
 package DepasqualeAndreaRepository.progettoSettimanaleJavaSecurity.users;
 
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.github.javafaker.Faker;
+
 import DepasqualeAndreaRepository.progettoSettimanaleJavaSecurity.users.dispositivi.DispositivoService;
+import DepasqualeAndreaRepository.progettoSettimanaleJavaSecurity.users.payload.UtenteRequestPayload;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
@@ -17,16 +22,16 @@ public class UtenteRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		Faker faker = new Faker(new Locale("it"));
-//
-//		for (int i = 0; i < 30; i++) {
-//			String name = faker.name().firstName();
-//			String surname = faker.name().lastName();
-//			String email = faker.internet().emailAddress();
-//			String password = faker.lorem().characters(6, 12);
-//			UtenteRequestPayload utente = new UtenteRequestPayload(name, surname, email, password);
-//			// utenteService.creaUtente(utente);
-//		}
+		Faker faker = new Faker(new Locale("it"));
+
+		for (int i = 0; i < 30; i++) {
+			String name = faker.name().firstName();
+			String surname = faker.name().lastName();
+			String email = faker.internet().emailAddress();
+			String password = faker.lorem().characters(6, 12);
+			UtenteRequestPayload utente = new UtenteRequestPayload(name, surname, email, password);
+			utenteService.creaUtente(utente);
+		}
 //		try {
 //
 //			UtenteRequestPayload dottore = new UtenteRequestPayload("mio", "dio", "asntocielo@Gmail.com",
